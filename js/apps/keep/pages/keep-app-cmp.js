a193460e6de265a720c482fbe96d82323b526200
Yonik94 <yehonatan121233@gmail.com>
2020-06-24 19:50:59 +0300

    keep app with input

--- js/apps/keep/pages/keep-app-cmp.js
+++ js/apps/keep/pages/keep-app-cmp.js
@@ -5,6 +5,7 @@ export default {
     name: 'keep-app',
     template:
         `<main>
+            <input type="text" placeholder="enter your note text"/>
             <notes-list :notes="notes"></notes-list>
         </main>`,
 
