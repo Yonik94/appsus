49ca6b83b60d3521b97c9a3280483d7837693a6e
Yonik94 <yehonatan121233@gmail.com>
2020-06-26 11:52:01 +0300

    emails sent & renderd

--- js/apps/email/cmps/emails-list-cmp.js
+++ js/apps/email/cmps/emails-list-cmp.js
@@ -1,9 +1,8 @@
 import { emailService } from '../services/email-service.js';
 import emailPreview from './email-preview-cmp.js';
 import emailDetails from './email-details-cmp.js';
+import emailCompose from './email-compose-cmp.js';
 import { eventBus } from '../../../services/event-bus-service.js';
-
-eventBus.$on('emailSent', (emails) => emails)
 
 
 // <email-compose v-else-if="$route.name === 'drafts'" :emailId="emailId"></email-compose>
@@ -12,10 +11,11 @@ export default {
     template:
         `<main>
             <ul v-if="!emailId && $route.name !== 'drafts'">
-                <router-link @emailSent="updateEmails(e)" v-for="email in emails" :key="email.emailId" :to="$route.name + '/' + email.emailId">
+                <router-link v-for="(email, idx) in emails" :key="idx" :to="$route.name + '/' + email.emailId">
                     <email-preview :email="email"></email-preview>
                 </router-link>
             </ul>
+            <email-compose v-else-if="$route.name === 'drafts'" :emailId="emailId"></email-compose>
             <email-details v-else :emailId="emailId"></email-details>
             </main>`,
     data() {
@@ -25,9 +25,11 @@ export default {
         }
     },
     created() {
+        eventBus.$on('emailSent', () => {
+            this.updateEmails() })
         this.emailId = this.$route.params.emailId;
         emailService.query(this.$route.name)
-            .then(emails => this.emails = emails);
+            .then(emails => this.emails = emails) 
     },
     watch: {
         '$route'(to, from) {
@@ -37,12 +39,14 @@ export default {
         }
     },
     methods: {
-        updateEmails(e){
-            console.log(e)
+        updateEmails(){
+            this.emails = emailService.query(this.$route.name)
+            .then(emails => this.emails = emails);
         }
     },
     components: {
         emailPreview,
-        emailDetails
+        emailDetails,
+        emailCompose
     }
 }
